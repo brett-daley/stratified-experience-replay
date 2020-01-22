732c9734ed662f630d294fac2c7b2300caf9afeb
Brett Daley <brett-daley@users.noreply.github.com>
2020-01-22 18:24:35 -0500

    Initial commit

---
+++ atari_env.py
@@ -0,0 +1,15 @@
+import gym
+from gym.wrappers import AtariPreprocessing
+import numpy as np
+
+
+def make(env_name):
+    env = gym.make(env_name)
+    env = AtariPreprocessing(env, terminal_on_life_loss=True)
+    env = ClippedRewardWrapper(env)
+    return env
+
+
+class ClippedRewardWrapper(gym.RewardWrapper):
+    def reward(self, reward):
+        return np.sign(reward)
