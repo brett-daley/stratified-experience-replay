ad0401ac526381e132ce10514a9a3a5bd68d8daa
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2020-02-27 03:28:26 -0500

    Added folder_path var to function

--- get_game_imgs.py
+++ get_game_imgs.py
@@ -3,10 +3,10 @@ import os
 from PIL import Image
 
 
-def take_pics(num_images, env_name):
+def take_pics(num_images, env_name, folder_path='game_images/'):
     # Check if images folder exists; if not, create one
-    if not os.path.exists('game_images'):
-        os.mkdir('game_images')
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
 
     # Create environment and use wrappers in atari_env.py
     env = atari_env.make(env_name)
@@ -14,7 +14,7 @@ def take_pics(num_images, env_name):
 
     for i in range(num_images):
         # Save image to file
-        name_string = 'game_images/img_' + str(i+1) + '.png'
+        name_string = folder_path + 'img_' + str(i+1) + '.png'
         Image.fromarray((observation * 255).astype('uint8'), mode='RGB').save(name_string)
 
         # Take random action
@@ -27,4 +27,4 @@ def take_pics(num_images, env_name):
 
 
 if __name__ == '__main__':
-    take_pics(num_images=1_000_000, env_name='PongNoFrameskip-v4')
+    take_pics(num_images=1_000_000, env_name='PongNoFrameskip-v4', folder_path='game_images/')
