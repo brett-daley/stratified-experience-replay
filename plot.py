367e0d3d46f1730735e11c812ff52853fddc2d0b
Brett Daley <brett-daley@users.noreply.github.com>
2020-04-08 15:52:53 -0400

    Write script to parse results and plot them

---
+++ plot.py
@@ -0,0 +1,132 @@
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+import numpy as np
+import re
+import os
+import yaml
+from glob import glob
+
+METRICS = ('timestep', 'episode', 'avg_return', 'epsilon')
+
+
+def new_report():
+    report = dict(seeds=0)
+    for m in METRICS:
+        report[m] = []
+    return report
+
+
+def grab(pattern, string):
+    assert '()' in pattern
+    pattern = pattern.replace('()', '(.*?)(?=_|$)')
+    return re.search(pattern, string).group(1)
+
+
+def get_data_lines(path):
+    pattern = re.compile(r'^[0-9]+\s{2}')
+    lines = []
+    with open(path, 'r') as fh:
+        for l in fh.readlines():
+            if re.match(pattern, l):
+                lines.append(l)
+    return lines
+
+
+def get_config():
+    with open('plot_config.yaml', 'r') as fh:
+        return yaml.safe_load(fh.read())
+
+
+def load_data_from_file(path):
+    lines = get_data_lines(path)
+    data = [np.fromstring(l, dtype=float, sep=' ') for l in lines]
+    return np.asarray(data)
+
+
+def parse_one(directory, key):
+    pattern = key + '_seed-*.txt'
+    files = glob(os.path.join(directory, pattern))
+    if not files:
+        print(f'Warning: skipping {key} because no files in {directory} match {pattern}')
+        return None
+
+    # {metric0: [seed0, seed1, ...], metric1: [seed0, seed1, ...], ...}
+    report = new_report()
+
+    for f in files:
+        data = load_data_from_file(f)
+        for i, m in enumerate(METRICS):
+            try:
+                report[m].append(data[:,i])
+            except IndexError:
+                print(f"Error: metric '{m}' could not be parsed from {f}")
+                print( '       (file is probably corrupted)')
+                raise
+        report['seeds'] += 1
+
+    # Average over seeds
+    for m in METRICS:
+        data = report.pop(m)
+
+        try:
+            mean = np.mean(data, axis=0)
+            if report['seeds'] > 1:
+                std = np.std(data, axis=0, ddof=1)
+            else:
+                std = np.zeros_like(mean)
+        except ValueError as e:
+            print(f'Warning: skipping {key} due to missing data')
+            print(f'         {str(e)}')
+            return None
+
+        report[m] = mean
+        report[m + '.std'] = std
+
+    return report
+
+
+def save(name, directory, pdf):
+    path = os.path.join(directory, name)
+    if pdf:
+        path += '.pdf'
+        plt.savefig(path, format='pdf')
+    else:
+        path += '.png'
+        plt.savefig(path, format='png')
+    print(f'Saved plot as {path}', flush=True)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--input_dir', type=str, default='results')
+    parser.add_argument('--output-dir', type=str, default='plots')
+    parser.add_argument('--pdf', action='store_true')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output_dir):
+        os.mkdir(args.output_dir)
+    easy_save = lambda name: save(name, args.output_dir, args.pdf)
+
+    config = get_config()
+
+    for plot_name, experiments in config.items():
+        plt.figure()
+
+        for e in experiments:
+            assert '_seed-' not in e
+
+            report = parse_one(args.input_dir, e)
+            x = report['timestep']
+            y = report['avg_return']
+            x, y = map(np.array, [x, y])
+            plt.plot(x, y, label=e)
+
+        plt.xlim([0, 3e6])
+        plt.legend(loc='best', framealpha=1.0)
+
+        easy_save(plot_name)
+        plt.close()
+
+
+if __name__ == '__main__':
+    main()
