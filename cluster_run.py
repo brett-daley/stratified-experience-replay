93441e412d3462f30865d9de0c4b2107cf8413e2
Brett Daley <brett-daley@users.noreply.github.com>
2020-04-22 21:23:51 -0400

    Update experiments and add auto timing

--- cluster_run.py
+++ cluster_run.py
@@ -1,30 +1,30 @@
 import argparse
 import subprocess
 import os.path
+import math
 
 ######### RUN PARAMETERS #########
-env_grid = ['breakout']
-n_grid = [1, 3, 5, 7, 9]
-m_grid = [2_500, 7_500, 12_500, 17_500, 22_500]
-seed_grid = [0]
-batch_flag_grid = [False]
+env_grid = ['beam_rider', 'breakout', 'pong', 'seaquest', 'space_invaders']
+n_grid = [1, 3, 5, 7]
+seed_grid = range(1)
+batch_flag_grid = [False, True]
 
 # CAUTION: Changes in timesteps will NOT be reflected in output/err file names
 timesteps = 5_000_000
 ##################################
 
 
-def dispatch(out_file, err_file, cmd, go):
+def dispatch(out_file, err_file, cmd, max_hours, go):
     """
     Populates 'runscript.sh' file to run 'dqn_original.py' file
-    on cluster's GPU partition for 4 hours with 1 node, 1 core, and 32GB memory
+    on cluster's GPU partition for 'max_hours' hours with 1 node, 1 core, and 32GB memory
     """
     with open('runscript.sh', 'w+') as f:
         f.write(
             f"""#!/bin/bash
 #SBATCH -n 1                 # Number of cores
 #SBATCH -N 1                 # Ensure that all cores are on one machine
-#SBATCH -t 0-04:00           # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t {format_time(max_hours)}           # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p gpu               # Partition to submit to
 #SBATCH --gres=gpu           # number of GPUs (here 1; see also --gres=gpu:n)
 #SBATCH --mem=32000          # Memory pool for all cores (see also --mem-per-cpu)
@@ -37,6 +37,20 @@ source activate openaigym  # Switch to openaigym conda environment
         )
     if go:
         subprocess.call(['sbatch', 'runscript.sh'])
+
+
+def format_time(total_hours):
+    '''Converts hours to D-HH:MM format.'''
+    days = total_hours // 24
+    frac_hour, hours = math.modf(total_hours % 24)
+    minutes = math.ceil(frac_hour * 60.0)
+    if minutes == 60:
+        hours += 1
+        minutes = 0
+    if hours == 24:
+        hours = 0
+        days += 1
+    return f'{int(days)}-{int(hours):02d}:{int(minutes):02d}'
 
 
 def print_red(string):
@@ -54,8 +68,11 @@ def main():
 
     for env in env_grid:
         for n in n_grid:
-            for m in m_grid:
-                for batch_flag in batch_flag_grid:
+            for batch_flag in batch_flag_grid:
+                # This ensures that the total amount of computation is the same
+                # for a fixed n-value, with or without batchmode enabled.
+                m_grid = [2500*n for n in n_grid] if not batch_flag else [2500]
+                for m in m_grid:
                     for seed in seed_grid:
                         # Generate file paths and executable command
                         env_no_underscore = env.replace("_", "")  # Reformat env name for output file name
@@ -71,10 +88,12 @@ def main():
 
                         # Otherwise, generate and run script on cluster
                         # Populates 'runscript.sh' file to run 'dqn_original.py' file
-                        # on cluster's GPU partition for 4 hours with 1 node, 1 core, and 32GB memory
+                        # on cluster's GPU partition with 1 node, 1 core, and 32GB memory
                         # Dispatches 'runscript.sh' to SLURM if '--go' flag was specified in CLI
                         print(basename)
-                        dispatch(out_file, err_file, cmd, args.go)
+                        # Timing heuristic: 3 hours per 2500 minibatches/epoch, plus an extra hour
+                        hours = (3*n + 1) if batch_flag else (3*m/2500 + 1)
+                        dispatch(out_file, err_file, cmd, hours, args.go)
 
     if not args.go:
         print_yellow('''
