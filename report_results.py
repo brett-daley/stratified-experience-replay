118169d7c0301d77d3a9ef12e67aa2976a48f4e7
Brett Daley <brett-daley@users.noreply.github.com>
2020-05-19 17:22:04 -0400

    Share parsing code

--- report_results.py
+++ report_results.py
@@ -7,81 +7,21 @@ import yaml
 from glob import glob
 import pandas as pd
 
-METRICS = ('timestep', 'episode', 'avg_return', 'epsilon', 'hours')
+from plot import grab, parse_one
 
 
-def new_report():
-    report = dict(seeds=0)
-    for m in METRICS:
-        report[m] = []
-    return report
+def parse_all(directory):
+    keys = set()
+    for f in os.listdir(directory):
+        k = re.sub('_seed-[0-9]+.txt', '', f)
+        keys.add(k)
 
-
-def get_data_lines(path):
-    pattern = re.compile(r'^[0-9]+\s{2}')
-    lines = []
-    with open(path, 'r') as fh:
-        for l in fh.readlines():
-            if re.match(pattern, l):
-                lines.append(l)
-    return lines
-
-
-def get_config():
-    with open('plot_config.yaml', 'r') as fh:
-        return yaml.safe_load(fh.read())
-
-
-def load_data_from_file(path):
-    lines = get_data_lines(path)
-    data = [np.fromstring(l, dtype=float, sep=' ') for l in lines]
-    return np.asarray(data)
-
-
-def parse_one(directory, key):
-    pattern = key + '_seed-*.txt'
-    files = glob(os.path.join(directory, pattern))
-    if not files:
-        print(f'Warning: skipping {key} because no files in {directory} match {pattern}')
-        return None
-
-    # {metric0: [seed0, seed1, ...], metric1: [seed0, seed1, ...], ...}
-    report = new_report()
-
-    for f in files:
-        data = load_data_from_file(f)
-        for i, m in enumerate(METRICS):
-            try:
-                report[m].append(data[:, i])
-            except IndexError:
-                print(f"Error: metric '{m}' could not be parsed from {f}")
-                print('       (file is probably corrupted)')
-                raise
-        report['seeds'] += 1
-
-    # Average over seeds
-    for m in METRICS:
-        data = report.pop(m)
-
-        try:
-            mean = np.mean(data, axis=0)
-            if report['seeds'] > 1:
-                std = np.std(data, axis=0, ddof=1)
-            else:
-                std = np.zeros_like(mean)
-        except ValueError as e:
-            print(f'Warning: skipping {key} due to missing data')
-            print(f'         {str(e)}')
-            return None
-
-        report[m] = mean
-        report[m + '.std'] = std
-
-    return report
-
-
-def print_yellow(string):
-    print('\033[1;33;40m' + string + '\033[0;37;40m')
+    summary = {}
+    for k in keys:
+        report = parse_one(directory, k)
+        if report is not None:
+            summary.update({k: report})
+    return summary
 
 
 def build_df(game_list, score_list, experiment_list):
@@ -139,24 +79,22 @@ def main():
     if not os.path.exists(args.output_dir):
         os.mkdir(args.output_dir)
 
-    config = get_config()
     game_list = []
-    e_list = []
+    exp_list = []
     score_list = []
 
-    for plot_name, experiments in config.items():
+    summary = parse_all(args.input_dir)
+    for exp_name, report in summary.items():
+        assert '_seed-' not in exp_name
 
-        for e in experiments:
-            assert '_seed-' not in e
+        game = grab('env-()', exp_name)
+        y = report['avg_return']
 
-            report = parse_one(args.input_dir, e)
-            y = report['avg_return']
+        game_list.append(game)
+        exp_list.append(exp_name)
+        score_list.append(np.nanmean(y))
 
-            game_list.append(plot_name)
-            e_list.append(e)
-            score_list.append(np.nanmean(y))
-
-    df = build_df(game_list, score_list, e_list)
+    df = build_df(game_list, score_list, exp_list)
 
     output_df(df, avg_return_flag=args.all_avg_returns, median_flag=args.game_medians, mean_flag=args.game_means)
 
