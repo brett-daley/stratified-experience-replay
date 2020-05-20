05007bff8400114f9e4f1819ec04d2c9062b09d9
Cameron Hickert <cameron_hickert@hks.harvard.edu>
2020-05-19 20:30:21 -0400

    updated results reporting to output by parameter combinations

--- report_results.py
+++ report_results.py
@@ -30,12 +30,6 @@ def build_df(game_list, score_list, experiment_list):
 
 
 def output_df(dataframe, avg_return_flag=False, median_flag=False, mean_flag=False):
-    if avg_return_flag is False and median_flag is False and mean_flag is False:
-        print_yellow(
-            """
-        No flag specified. To view results, add --all_avg_returns and/or --game_medians and/or --game_means
-            """
-        )
 
     if avg_return_flag:
         df_sorted = dataframe.sort_values(by=['game', 'avg_return'], ascending=[True, False])
@@ -71,8 +65,11 @@ def main():
     parser.add_argument('--game_means', action='store_true',
                         help='(flag) Displays mean of all normalized scores for a given game, across all games. \
                                            Default disabled')
+    parser.add_argument('--perf_by_params', action='store_true',
+                        help='(flag) Displays mean of all normalized scores for parameter set, across all games. \
+                        Default disabled')
     parser.add_argument('--input_dir', type=str, default='results')
-    parser.add_argument('--output-dir', type=str, default='plots')
+    parser.add_argument('--output_dir', type=str, default='plots')
     parser.add_argument('--pdf', action='store_true')
     args = parser.parse_args()
 
@@ -82,6 +79,7 @@ def main():
     game_list = []
     exp_list = []
     score_list = []
+    param_list = []
 
     summary = parse_all(args.input_dir)
     for exp_name, report in summary.items():
@@ -89,14 +87,46 @@ def main():
 
         game = grab('env-()', exp_name)
         y = report['avg_return']
+        params = re.findall('n-[0-9]_m-[0-9]', exp_name)[0]
 
         game_list.append(game)
         exp_list.append(exp_name)
         score_list.append(np.nanmean(y))
+        param_list.append(params)
 
-    df = build_df(game_list, score_list, exp_list)
+    if args.all_avg_returns is False \
+            and args.game_medians is False \
+            and args.game_means is False \
+            and args.perf_by_params is False:
+        print(
+            """
+        No flag specified. To view results, add --all_avg_returns and/or --game_medians and/or --game_means and/or --perf_by_params
+            """
+        )
 
-    output_df(df, avg_return_flag=args.all_avg_returns, median_flag=args.game_medians, mean_flag=args.game_means)
+    if args.perf_by_params is True:
+
+        df_base = pd.DataFrame(zip(game_list, score_list), index=param_list, columns=['game', 'avg_return'])
+        df_reformat = pd.DataFrame(index=list(set(param_list)), columns=list(set(game_list)))
+
+        for index, row in df_base.iterrows():
+            df_reformat.at[index, row['game']] = row['avg_return']
+
+        df3_adjusted = df_reformat / df_reformat.max()
+        df3_total = df3_adjusted.copy()
+        df3_total['mean'] = df3_adjusted.mean(axis=1)
+        df3_total['median'] = df3_adjusted.median(axis=1)
+
+        df3_finalsort = df3_total.sort_values(by=['mean'], ascending=[False])
+
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+            # print(df3_mean)
+            print(df3_finalsort)
+            df3_finalsort.to_csv(args.output_dir + '/' + 'results_tables.csv')
+
+    if args.all_avg_returns is True or args.game_medians is True or args.game_means is True:
+        df = build_df(game_list, score_list, exp_list)
+        output_df(df, avg_return_flag=args.all_avg_returns, median_flag=args.game_medians, mean_flag=args.game_means)
 
 
 if __name__ == '__main__':
