661e6e1246013006cfb262e7681d01d3cc932f53
Brett Daley <brett-daley@users.noreply.github.com>
2020-07-08 23:36:34 -0400

    Implement multiplexer and test on CartPole

--- dqn_utils/models.py
+++ dqn_utils/models.py
@@ -1,4 +1,6 @@
+import tensorflow as tf
 from tensorflow.keras.layers import Conv2D, Dense, Flatten, InputLayer
+from tensorflow.keras.models import Sequential
 
 
 def get_model_fn_by_name(model_name):
@@ -16,3 +18,37 @@ def atari_cnn():
 def cartpole_mlp():
     return [Dense(512, activation='tanh'),
             Dense(512, activation='tanh')]
+
+
+def cartpole_mlp_2mux():
+    submodel_fn = lambda: Sequential([Dense(361, activation='tanh') for _ in range(2)])
+    return [Multiplexer(submodel_fn, n=2)]
+
+
+def cartpole_mlp_3mux():
+    submodel_fn = lambda: Sequential([Dense(295, activation='tanh') for _ in range(2)])
+    return [Multiplexer(submodel_fn, n=3)]
+
+
+def cartpole_mlp_10mux():
+    submodel_fn = lambda: Sequential([Dense(160, activation='tanh') for _ in range(2)])
+    return [Multiplexer(submodel_fn, n=10)]
+
+
+class Multiplexer(tf.keras.Model):
+    def __init__(self, submodel_fn, n):
+        super().__init__()
+        self.submodels = [submodel_fn() for _ in range(n)]
+        self.n = n
+
+    def call(self, inputs):
+        outputs = tf.stack([m(inputs) for m in self.submodels])
+        return multiplex(outputs)
+
+
+def multiplex(all_outputs):
+    stacked_outputs = tf.stack(all_outputs)
+    activations = tf.reduce_sum(tf.abs(stacked_outputs), axis=-1)
+    selection_mask = tf.one_hot(tf.argmax(activations, axis=0), depth=len(all_outputs))
+    selection_mask = tf.transpose(selection_mask)[..., None]
+    return tf.reduce_sum(selection_mask * stacked_outputs, axis=0)
