96e41f23015f2cea7eb02b55c92a044af9072a02
Brett Daley <brett-daley@users.noreply.github.com>
2020-09-22 18:58:38 -0400

    Revert to custom exploration

--- dqn_utils/hparams.py
+++ dqn_utils/hparams.py
@@ -19,10 +19,10 @@ def get_hparams(env_name):
 def atari_hparams():
     return {
         'discount': 0.99,
-        'epsilon_schedule': schedules.LinearAnnealSchedule(start_value=1.0, end_value=0.1, timeframe=1_000_000),
+        'epsilon_schedule': schedules.ConstantSchedule(0.1),
         'model_fn': get_model_fn_by_name('atari_cnn'),
         'optimizer': optimizers.Adam(learning_rate=1e-4, epsilon=1e-4),
-        'prepopulate': 50_000,
+        'prepopulate': 250_000,
         'rmem_constructor': lambda env: ReplayMemory(env, batch_size=32, capacity=1_000_000),
         'scale_obs': 1.0 / 255.0,
         'update_freq': 10_000,
