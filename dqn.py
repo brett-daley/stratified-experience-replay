fccebb239e42072efcd17f0bad30104805e1cee4
Cameron Hickert <chickert@boslogin04.rc.fas.harvard.edu>
2021-01-14 02:12:25 -0500

    added wandb arg to dqn.py

--- dqn.py
+++ dqn.py
@@ -147,6 +147,7 @@ def train(env, agent, prepopulate, epsilon_schedule, timesteps):
     observation = env.reset()
 
     print('episode', 'timestep', 'return', 'avg_return', 'epsilon', 'hours', sep='  ', flush=True)
+    episodes = 0
     start_time = time.time()
 
     for t in range(-prepopulate, timesteps+1):  # Relative to training start
@@ -172,10 +173,15 @@ def train(env, agent, prepopulate, epsilon_schedule, timesteps):
         if done:
             new_observation = env.reset()
 
-            # New log: every episode completion, to output file only
+            # Some environments (e.g. Pong) signal "done" when the player loses a life,
+            # so make sure this is a real episode termination
             rewards = env.get_episode_rewards()
-            hours = (time.time() - start_time) / 3600
-            print(f'{len(rewards)}  {t}  {rewards[-1]}  {np.mean(rewards[-100:])}  {epsilon:.3f}  {hours:.3f}', flush=True)
+            if len(rewards) != episodes:
+                episodes = len(rewards)
+
+                # New log: every episode completion, to output file only
+                hours = (time.time() - start_time) / 3600
+                print(f'{len(rewards)}  {t}  {rewards[-1]}  {np.mean(rewards[-100:])}  {epsilon:.3f}  {hours:.3f}', flush=True)
 
         agent.save(observation, action, reward, done, new_observation)
         observation = new_observation
@@ -203,15 +209,13 @@ if __name__ == '__main__':
                         help='(int) Seed for random number generation. Default: 0')
     parser.add_argument('--rmem_type', type=str, default='StratifiedReplayMemory',
                         help='(str) Name of replay memory class. Default: StratifiedReplayMemory')
-    parser.add_argument('--wandb_proj', type=str, default='SER-final',
-                        help='(str) Name of Weights & Biases project. Default: SER-final')
     args = parser.parse_args()
 
     tf.random.set_seed(args.seed)
     np.random.seed(args.seed)
     random.seed(args.seed)
 
-    wandb.init(project=args.wandb_proj, name=args.env + '-' + args.rmem_type)
+    wandb.init(project="frozenlake", name="picky rmem")
     env = dqn_utils.make_env(args.env, args.seed)
     hparams = dqn_utils.get_hparams(args.env)
     hparams['timesteps'] = args.timesteps
